9bd9f09e038c3fbd44ea3a171cd05467d877f7c8
Will Bush <williamdcbush@gmail.com>
2019-11-04 15:04:13 +0000

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,68 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      stocks: [],
+      portfolio: [],
+      display: []
+    }
+  }
+  
+
+  // let API = "http://localhost:3003/stocks"
+
+  componentDidMount() {
+    fetch("http://localhost:3003/stocks")
+    .then(resp => resp.json())
+    .then(stocksData => this.setState({
+      stocks: stocksData,
+      display: stocksData
+    }))
+  }
+
+addToPortfolio = (stock) => {
+this.setState({
+  portfolio: [...this.state.portfolio, stock]
+})
+  }
+
+  removeFromPortfolio = (stock) => {
+    this.setState({
+      portfolio: this.state.portfolio.filter(s => s !== stock) 
+    })
+  }
+
+  sortStocks = (sortBy) => {
+    let sortedStocks = []
+    switch(sortBy) {
+      case "Alphabetically":
+          sortedStocks = this.state.display.sort((a,b) => a.name > b.name ? 1: -1)
+        break;
+        case "Price":
+            sortedStocks = this.state.display.sort((a,b) => a.price > b.price ? 1 : -1)
+          break;
+    }
+    this.setState({
+      display: sortedStocks
+    })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sort={this.sortStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.display} add={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolio} remove={this.removeFromPortfolio}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -8,7 +8,9 @@ class PortfolioContainer extends Component {
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.props.stocks.map(stock => (
+              <Stock key={stock.id }stock={stock} remove={this.props.remove}/>
+            ))
           }
       </div>
     );
