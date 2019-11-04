4ea8b987477b9294c9b9a44a4a80cc513854f263
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-04 22:16:48 +0000

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const handleSortChange = e => {
+    props.setSort(e.target.value)
+  }
+
+  const handleFilterChange = e => {
+    props.setFilter(e.target.value)
+  }
+
   return (
-    <div>
+    < div >
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Default" checked={props.sort === "Default" ? true : false} onChange={handleSortChange} />
+        Default
+      </label>
+      <label>
+        <input type="radio" value="Alphabetically" checked={props.sort === "Alphabetically" ? true : false} onChange={handleSortChange} />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sort === "Price" ? true : false} onChange={handleSortChange} />
         Price
       </label>
-      <br/>
+      <br />
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilterChange}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
@@ -25,7 +39,7 @@ const SearchBar = () => {
       </label>
 
 
-    </div>
+    </div >
   );
 }
 
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,13 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+
+    const stocks = this.props.sortedStocks
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          stocks.map(stock => <Stock stock={stock} key={stock.id} handleClick={() => this.props.buyStock(stock.id)} />)
         }
       </div>
     );
