532028d9314268caf4733a17dde799bdce482b6a
Abdullah Bajaman <ambajaman@gmail.com>
2019-11-05 13:29:25 +0000

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,19 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+    const stocks = this.props.stocks  
+    // const stocks = [...this.props.stocks].sort((a,b)=> a.name > b.name ? 1 : -1)
+
     return (
       <div>
         <h2>Stocks</h2>
         {
           //render the list of stocks here
+          stocks.map(stock => <Stock 
+            key={stock.id} 
+            stock={stock} 
+            addProtfolio={this.props.addProtfolio}
+            /> )
         }
       </div>
     );
